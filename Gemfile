eff3b78b5547127666806fa2e7c8dfdc8cda908c
Yannick PLACERDAT <yannick.placerdat@gmail.com>
2014-12-22 18:28:02 +0100

    App is ready !

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+gem 'rails', '4.0.5'
+
+gem 'pg'
+gem 'turbolinks'
+gem 'jquery-rails'
+gem 'haml-rails'
+gem 'sass-rails'
+gem 'compass'
+gem 'bootstrap-sass'
+gem 'autoprefixer-rails'
+gem 'coffee-rails'
+gem 'therubyracer', :platforms => :ruby
+gem 'uglifier'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IlFaro::Application.config.secret_key_base = 'e249669c4dfe97cb10c1297efe731482753565888b5eb418518849099cd677ad7296544cad680cd1f969922cb59b0169733f02daffee654f4bc111bedbf8a89c'

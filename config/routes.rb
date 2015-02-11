93dc3e2333e277c11d051372958d2ee68648f251
Yannick PLACERDAT <yannick.placerdat@gmail.com>
2015-02-11 13:39:15 +0100

    Base of menu page is in the place !

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 IlFaro::Application.routes.draw do
   root 'home#index'
+
+  get 'menu/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
